b57c18466fd3f45cc4020a07fc10e24aad475b54
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-09-18 15:02:51 +0300

    18.09.2022

---
+++ src/demoProjects/rentACar/Application/Features/Models/Profiles/MappingProfiles.cs
@@ -0,0 +1,20 @@
+﻿using Application.Features.Models.Dtos;
+using Application.Features.Models.Models;
+using AutoMapper;
+using Core.Persistence.Paging;
+using Domain.Entities;
+
+namespace Application.Features.Models.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Model, ModelListDto>()
+                .ForMember(x => x.BrandName, opt => opt.MapFrom(x => x.Brand.Name))
+                .ReverseMap();
+
+            CreateMap<IPaginate<Model>, ModelListModel>().ReverseMap();
+        }
+    }
+}
