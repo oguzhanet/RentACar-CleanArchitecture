5a278b3ed4a00063f93b1d3466a1ed47e6b9f62c
ogzhncengiz@outlook.com <ogzhncengiz@outlook.com>
2022-10-28 18:27:40 +0300

    28.10.2022

--- src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
+++ src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using Application.Features.Brands.Rules;
+﻿using Application.Features.Auths.Rules;
+using Application.Features.Brands.Rules;
+using Application.Services.AuthService;
 using Core.Application.Pipelines.Validation;
 using FluentValidation;
 using MediatR;
@@ -16,6 +18,7 @@ namespace Application
             services.AddMediatR(Assembly.GetExecutingAssembly());
 
             services.AddScoped<BrandBusinessRules>();
+            services.AddScoped<AuthBusinessRules>();
 
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>));
@@ -24,8 +27,9 @@ namespace Application
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
 
-            return services;
+            services.AddScoped<IAuthService,AuthManager>();
 
+            return services;
         }
     }
 }
